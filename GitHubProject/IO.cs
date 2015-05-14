559afd10eb3be4f40b3c0a5e850e24edc00be936
xinyue Shi <sxyloading@gmail.com>
2015-05-14 17:39:07 +0800

    首次上传工程

---
+++ GitHubProject/IO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GitHubProject
+{
+    struct Turple {
+        String name;
+        String value;
+    }
+    class TestUnit {
+        public Turple ID;
+        public Turple Name;
+    }
+    public class IO
+    {
+        //target load data from file 
+        public Turple[] ReadData(FileStream file) {
+
+
+        }
+    }
+}
